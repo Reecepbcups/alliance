7e1eebadfc6352a13cab07cbbe6bd60e4262901b
Reece Williams <reecepbcups@gmail.com>
2024-10-03 16:57:56 -0500

    fix: add `cosmos.msg.v1.service`

--- x/alliance/types/query.pb.go
+++ x/alliance/types/query.pb.go
@@ -1931,6 +1931,7 @@ func _Query_Alliance_Handler(srv interface{}, ctx context.Context, dec func(inte
 	return interceptor(ctx, in, info, handler)
 }
 
+var Query_serviceDesc = _Query_serviceDesc
 var _Query_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "alliance.alliance.Query",
 	HandlerType: (*QueryServer)(nil),
